0038f8dcaedea52cc92693784bc1835540effcb9
Christian <s194255@student.dtu.dk>
2023-12-12 09:28:06 +0100

    ændringer

---
+++ src/ground_helper.js
@@ -0,0 +1,81 @@
+function InitGround(){
+    gl.useProgram( groundProgram );
+    modelGround = new Object();
+    CreateMVPTeapotGround();
+    InitGroundTexture(gl);
+    createRectangle();
+}
+
+function InitGroundTexture(gl){
+    imageTexture = gl.createTexture();
+    var TextureImage = document.createElement('img');
+    TextureImage.crossorigin = 'anonymous';
+    TextureImage.onload = function() {
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, imageTexture);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, TextureImage);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+        modelGround.texCoordsPerm4 = [
+            vec2(1, 0), vec2(0, 0), vec2(0, 1), vec2(1, 1)
+        ];                                                                                                                                                      
+        modelGround.texBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, modelGround.texBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(modelGround.texCoordsPerm4), gl.STATIC_DRAW);
+        groundProgram.vTexCoord = gl.getAttribLocation(groundProgram, "vTexCoord");
+        gl.vertexAttribPointer(groundProgram.vTexCoord, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(groundProgram.vTexCoord);
+
+    };
+    TextureImage.src = "xamp23.png";
+    gl.uniform1i(gl.getUniformLocation(groundProgram, "texture"), 0);
+}
+
+function createRectangle(){
+    modelGround.vertices = [
+        vec4(-2, -1, -1, 1.0), vec4(2, -1, -1, 1.0), vec4(2, -1, -5, 1.0), vec4(-2, -1, -5, 1.0),
+        vec4(-1.0, -1.0, -2.5, 1.0), vec4(-1.0, 0.0, -2.5, 1.0), vec4(-1.0, 0.0, -3.0, 1.0), vec4(-1.0, -1.0, -3.0, 1.0),
+        vec4(0.25, -0.5, -1.25, 1.0), vec4(0.75, -0.5, -1.25, 1.0), vec4(0.75, -0.5, -1.75, 1.0), vec4(0.25, -0.5, -1.75, 1.0),
+    ]
+    modelGround.indices = new Uint32Array(
+        [
+            0, 1, 2, 0, 2, 3,
+            4, 5, 6, 4, 6, 7,
+            8, 9, 10, 8, 10, 11
+        ]);
+
+    modelGround.iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, modelGround.iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(modelGround.indices), gl.STATIC_DRAW);
+
+    modelGround.vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, modelGround.vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(modelGround.vertices), gl.STATIC_DRAW);
+    groundProgram.vPosition = gl.getAttribLocation(groundProgram, "a_Position");
+    gl.vertexAttribPointer(groundProgram.vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(groundProgram.vPosition);
+
+    num_indices_quad = 12;
+    num_indices_ground = 6;
+}
+
+function CreateMVPTeapotGround(){
+    var PLoc = gl.getUniformLocation(groundProgram, "P");
+    var P = perspective(45, 1, 1, 10);
+    gl.uniformMatrix4fv(PLoc, false, flatten(P));
+
+    var VLoc = gl.getUniformLocation(groundProgram, "V");
+    var eyeLoc = gl.getUniformLocation(groundProgram, "eye");
+    var at = vec3(0, -1, -10);
+    var eye = add(at, vec3(10, 10, 10));
+    var up = vec3(0.0, 1.0, 0.0);
+    // var V = lookAt(eye, at, up);
+    var V = mat4();
+    gl.uniformMatrix4fv(VLoc, false, flatten(V));
+    gl.uniform3fv(eyeLoc, eye);
+}
---
+++ src/teapot_helper.js
@@ -0,0 +1,158 @@
+var Mo;
+var lightPos;
+var motion = 1.0;
+function onReadOBJFile(fileString, fileName, gl, o, scale, reverse){
+    var objDoc = new OBJDoc(fileName); // Create a OBJDoc object
+    var result = objDoc.parse(fileString, scale, reverse);
+    if (!result) {
+        g_objDoc = null; g_drawingInfo = null;
+        console.log("OBJ file parsing error.");
+        return;
+    }
+    g_objDoc = objDoc;
+}
+function onReadComplete(gl, model, objDoc) {
+    // Acquire the vertex coordinates and colors from OBJ file
+    var drawingInfo = objDoc.getDrawingInfo();
+    // Write date into the buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, model.vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, drawingInfo.vertices,gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, model.normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, drawingInfo.normals, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, model.colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, drawingInfo.colors, gl.STATIC_DRAW);
+    // Write the indices to the buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, drawingInfo.indices, gl.STATIC_DRAW);
+    return drawingInfo
+}
+
+function initObject(gl, program, obj_filename, scale)
+{
+    program.a_Position = gl.getAttribLocation(program, 'a_Position');
+    program.a_Normal = gl.getAttribLocation(program, 'a_Normal');
+    program.a_Color = gl.getAttribLocation(program, 'a_Color');
+    // Prepare empty buffer objects for vertex coordinates, colors, and normals
+    var model = initVertexBuffers(gl, program);
+    // Start reading the OBJ file
+    readOBJFile(obj_filename, gl, model, scale, true);
+    return model;
+}
+
+function initVertexBuffers(gl, program) {
+    var o = new Object();
+    o.vertexBuffer = createEmptyArrayBuffer(gl, program.a_Position, 3, gl.FLOAT);
+    // o.vertexBuffer = createEmptyArrayBuffer(gl, program.a_Position, 4, gl.FLOAT);
+    o.normalBuffer = createEmptyArrayBuffer(gl, program.a_Normal, 3, gl.FLOAT);
+    o.colorBuffer = createEmptyArrayBuffer(gl, program.a_Color, 4, gl.FLOAT);
+    o.indexBuffer = gl.createBuffer();
+    return o;
+}
+function createEmptyArrayBuffer(gl, a_attribute, num, type) {
+    var buffer = gl.createBuffer(); // Create a buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+    gl.enableVertexAttribArray(a_attribute); // Enable the assignment
+    return buffer;
+}
+function readOBJFile(fileName, gl, model, scale, reverse) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status !== 404) {
+            onReadOBJFile(request.responseText, fileName, gl, model, scale, reverse);
+        }
+    }
+    request.open('GET', fileName, true); // Create a request to get file
+    request.send(); // Send the request
+}
+function CreateMVPTeapot(){
+    var PLoc = gl.getUniformLocation(teapotProgram, "P");
+    var P = perspective(45, 1, 1, 10);
+    gl.uniformMatrix4fv(PLoc, false, flatten(P));
+
+    var VLoc = gl.getUniformLocation(teapotProgram, "V");
+    var eyeLoc = gl.getUniformLocation(teapotProgram, "eye");
+    var at = vec3(0, -1, -10);
+    var eye = add(at, vec3(10, 10, 10));
+    var up = vec3(0.0, 1.0, 0.0);
+    // var V = lookAt(eye, at, up);
+    var V = mat4();
+    gl.uniformMatrix4fv(VLoc, false, flatten(V));
+    gl.uniform3fv(eyeLoc, eye);
+
+    var LaLoc = gl.getUniformLocation(teapotProgram, "La");
+    var La = vec3(1.0, 1.0,1.0);
+    gl.uniform3fv(LaLoc, La);
+
+    // var MLoc = gl.getUniformLocation(teapotProgram, "M");
+    // modelTeapot.M = translate(0, -1, -3);
+    // gl.uniformMatrix4fv(MLoc, false, flatten(modelTeapot.M));
+
+    Mo = mat4();
+}
+
+function createSlidersTeapot(){
+    var kdRange = document.getElementById("kdRange");
+    var kaRange = document.getElementById("kaRange");
+    var ksRange = document.getElementById("ksRange");
+    var sRange = document.getElementById("sRange");
+    var LiRange = document.getElementById("LiRange");
+    var toggleMotion = document.getElementById("toggleMotion");
+    var k_dLoc = gl.getUniformLocation(teapotProgram, "k_d");
+    var k_aLoc = gl.getUniformLocation(teapotProgram, "k_a");
+    var LiLoc = gl.getUniformLocation(teapotProgram, "Li");
+    var k_sLoc = gl.getUniformLocation(teapotProgram, "k_s");
+    var sLoc = gl.getUniformLocation(teapotProgram, "s");
+
+    var k_d = parseFloat(kdRange.value);
+    gl.uniform1f(k_dLoc, k_d);
+    var k_a = parseFloat(kaRange.value);
+    gl.uniform1f(k_aLoc, k_a);
+    var k_s = parseFloat(ksRange.value);
+    gl.uniform1f(k_sLoc, k_s);
+    var s = parseFloat(sRange.value);
+    gl.uniform1f(sLoc, s);
+    var Li = parseFloat(LiRange.value);
+    gl.uniform3fv(LiLoc, vec3(Li, Li, Li));
+
+    kdRange.addEventListener("input", function () {
+        gl.useProgram( teapotProgram );
+        var k_d = parseFloat(kdRange.value);
+        gl.uniform1f(k_dLoc, k_d);
+    });
+
+    kaRange.addEventListener("input", function () {
+        gl.useProgram( teapotProgram );
+        var k_a = parseFloat(kaRange.value);
+        gl.uniform1f(k_aLoc, k_a);
+    });
+
+    ksRange.addEventListener("input", function () {
+        gl.useProgram( teapotProgram );
+        var k_s = parseFloat(ksRange.value);
+        gl.uniform1f(k_sLoc, k_s);
+    });
+
+    sRange.addEventListener("input", function () {
+        gl.useProgram( teapotProgram );
+        var s = parseFloat(sRange.value);
+        gl.uniform1f(sLoc, s);
+    });
+
+    LiRange.addEventListener("input", function () {
+        gl.useProgram( teapotProgram );
+        var Li = parseFloat(LiRange.value);
+        gl.uniform3fv(LiLoc, vec3(Li, Li, Li));
+    });
+
+    toggleMotion.addEventListener("click", function () {
+        motion = 1 - motion;
+    });
+}
+function initTeapot(){
+    gl.useProgram( teapotProgram );
+    CreateMVPTeapot();
+    createSlidersTeapot();
+    modelTeapot = initObject(gl, teapotProgram, "teapot.obj", 0.25);
+}
